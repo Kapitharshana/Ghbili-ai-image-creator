eb04cc2095265e38f76489ddb1938614b4d91076
Kapitharshana <kapidarsha431@gmail.com>
2026-02-22 00:17:04 +0530

    Update GenerationController.java

--- Ghibiliai/src/main/java/art/example/Ghibiliai/controller/GenerationController.java
+++ Ghibiliai/src/main/java/art/example/Ghibiliai/controller/GenerationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"})
+//@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"})
 @RequiredArgsConstructor
 public class GenerationController {
 
